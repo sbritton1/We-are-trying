33e57d673ee421c0dc6387c8705e04b378fbd682
thana3105 <masiewe@live.nl>
2023-01-11 11:26:26 +0100

    Update baseline.py

--- code/algorithms/baseline.py
+++ code/algorithms/baseline.py
@@ -1,7 +1,41 @@
 import random
+import matplotlib.pyplot as plt
+import copy
 
 def baseline(grid):
-    for house in grid.houses:
-        battery = random.choice(grid.batteries)
+
+    costs: list[int] = []
+
+    for i in range(1000):
+        tmp_grid = add_connections(grid)
+        cost = price(tmp_grid)
+        costs.append(cost)
+        
+    plot_cost(costs)
+
+
+def add_connections(grid):
+    tmp_grid = copy.deepcopy(grid)
+
+    for house in tmp_grid.houses:
+        battery = random.choice(tmp_grid.batteries)
         battery.connect_home(house)
         house.make_connection(battery)
+
+    return tmp_grid
+
+def price(tmp_grid):
+    cost: int = 0
+    cost += len(tmp_grid.batteries) * 5000
+    print(cost)
+
+    for house in tmp_grid.houses:
+        cost += house.distance_to_battery * 9
+    
+    print(f"Total cost = {cost}")
+
+    return cost
+
+def plot_cost(costs: list[int]):
+    plt.hist(costs)
+
