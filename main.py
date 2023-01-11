861d2db44b54a7de2aa51f8bd1d5687071b6f06d
thana3105 <masiewe@live.nl>
2023-01-11 13:48:03 +0100

    Update main.py
    removed redundant code

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ loc_type = list[tuple[int, int, float]]
 
 
 def main(district: str):
-    file_batteries: str = "data/district_" + district + "/district-" + district + "_batteries.csv"
-    file_houses: str= "data/district_" + district + "/district-" + district + "_houses.csv"
-
     grid = Grid(district)
 
     grid = baseline(grid)
