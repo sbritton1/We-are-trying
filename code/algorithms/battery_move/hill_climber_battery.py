cb4bad7608890a92ebfce693f2f6adcacdede0b6
thana3105 <masiewe@live.nl>
2023-01-24 14:19:17 +0100

    Update hill_climber_battery.py

--- code/algorithms/battery_move/hill_climber_battery.py
+++ code/algorithms/battery_move/hill_climber_battery.py
@@ -30,36 +30,22 @@ def init_hill_climber_battery(grid: Grid):
     # use multithread processing, with workers amount of threads
     workers = 1
     p = multiprocessing.Pool(workers)
-    results = (p.map(work, grids))
+    results = (p.map(hill_climber_battery, grids))
 
     return
 
-def work(grid: Grid):
-    """
-    Runs the simulated annealing and returns the
-    grid and costs.
 
-    Pre:  grid of class grid
-    Post: tuple containing grid of class grid and integer
-    """
-
-    # run algorithm and return the result
-    run_algo = hill_climber_battery(grid)
-    grid: Grid = run_algo[0]
-    costs = run_algo[1]
-    print(grid.cost)
-    return (grid, costs)
-
-def hill_climber_battery(grid: Grid):
+def hill_climber_battery(grid: Grid) -> tuple[Grid, list[int]]:
     org_cost: int = grid.cost
-    last_improvement = 0
-    iteration = 0
+    costs: list[int] = []
+    last_improvement: int = 0
+    iteration: int = 0
 
     while last_improvement < 0 and iteration < 1000:
         tmp_grid: Grid = copy.deepcopy(grid)
         tmp_grid.remove_cables()
         tmp_grid.remove_connections()
-        
+
         tmp_grid = move_battery(tmp_grid)
         tmp_grid = greedy(tmp_grid)
         tmp_grid.lay_shared_cables()
@@ -67,10 +53,15 @@ def hill_climber_battery(grid: Grid):
 
         if new_cost < org_cost:
             grid = tmp_grid
+            costs.append(new_cost)
             org_cost = new_cost
             last_improvement = iteration
         
         iteration += 1
+
+    print(costs[-1])
+    return grid, costs
+
 
 def move_battery():
     pass
