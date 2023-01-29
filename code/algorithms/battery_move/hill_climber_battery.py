fb63afe7027d191124f81b18eecd2bd4e8634c03
thana3105 <masiewe@live.nl>
2023-01-29 19:47:53 +0100

    added comments and docstrings to hilll_climber_batteries

--- code/algorithms/battery_move/hill_climber_battery.py
+++ code/algorithms/battery_move/hill_climber_battery.py
@@ -8,6 +8,13 @@ import random
 
 
 def init_hill_climber_battery(grid: Grid) -> Grid:
+    """
+    Initializes grids with random battery placements and greedy solutions.
+    Then calls function to move batteries to try and find a cheaper solution.
+
+    Pre : grid is of class Grid
+    Post: returns grid with best found placement of batteries
+    """
 
     # create list of grids as work for multithreading
     grids: list[Grid] = []
@@ -30,8 +37,11 @@ def init_hill_climber_battery(grid: Grid) -> Grid:
     p = multiprocessing.Pool(workers)
     results = (p.map(hill_climber_battery, grids))
 
+    # store cheapest found grid
     best_result: Grid = None
     best_costs: list[int] = [1000000]
+
+    # look through results to find cheapest solution
     for result in results:
         new_grid = result[0]
         costs = result[1]
@@ -40,29 +50,51 @@ def init_hill_climber_battery(grid: Grid) -> Grid:
             best_result = new_grid
 
     print(f"Best cost: {best_result.cost}")
+
+    # find possible improvements using hill climber algorithm
     best_result = init_hill_climber_shared(best_result, False)
 
     return best_result
 
 
 def randomize_battery_placement(grid: Grid) -> None:
+    """
+    Places batteries in random locations in the grid.
+
+    Pre : grid is of class Grid
+    Post: batteries have been placed on random coordinates in grid
+    """
+
     for battery in grid.batteries:
         new_x = -1
         new_y = -1 
         
+        # grid.move_battery() returns false when move cannot be made, so keep trying
         while grid.move_battery(battery, new_x, new_y) is False:
             new_x = random.choice(list(range(grid.size_grid()[0])))
             new_y = random.choice(list(range(grid.size_grid()[1])))
-        
 
 
 def hill_climber_battery(grid: Grid) -> tuple[Grid, list[int]]:
+    """
+    Runs a hill climber for the battery, gradually moving a battery
+    and checking if this results in a cheaper grid.
+
+    Pre : grid is of class Grid
+    Post: returns tuple of grid of cheapest found battery configuration and its
+          associated cost
+
+    """
+
     org_cost: int = grid.cost
     costs: list[int] = []
     last_improvement: int = 0
     iteration: int = 0
 
+    # allow for 100 iterations in which no improvements have been made
     while iteration - last_improvement < 100 and iteration < 1000:
+
+        # make copy of grid, move a random battery and lay the cables for it
         tmp_grid: Grid = copy.deepcopy(grid)
         move_battery(tmp_grid)
         tmp_grid.remove_all_connections()
@@ -70,6 +102,7 @@ def hill_climber_battery(grid: Grid) -> tuple[Grid, list[int]]:
         tmp_grid.lay_shared_cables()
         new_cost = tmp_grid.calc_cost_shared()
 
+        # replace grid if change creates cheaper solution
         if new_cost < org_cost:
             grid = tmp_grid
             costs.append(new_cost)
@@ -83,20 +116,41 @@ def hill_climber_battery(grid: Grid) -> tuple[Grid, list[int]]:
 
 
 def move_battery(grid: Grid) -> None:
+    """
+    Moves one random battery to a new location somewhere 
+    around its current location.
+
+    Pre : grid is of class Grid
+    Post: one battery moved a between 0 and 10 steps in each direction
+    """
+
     battery: Battery = random.choice(grid.batteries)
 
     target_x: int = battery.coord_x
     target_y: int = battery.coord_y
-    
+
+    # grid.move_battery() returns false when chosen location is already occupied
     while grid.move_battery(battery, target_x, target_y) is False:
-        target_x += random.choice(list(range(-10, 10, 1)))
+
+        # move battery random steps in both x and y directions
+        target_x = random.choice(list(range(-10, 10, 1)))
         target_x = track_size(grid, target_x, 0)
 
-        target_y += random.choice(list(range(-10, 10, 1)))
+        target_y = random.choice(list(range(-10, 10, 1)))
         target_y = track_size(grid, target_y, 1)
 
 
 def track_size(grid: Grid, coord: int, dir: int) -> int:
+    """
+    Checks if new coordinate is within the grid. If not, place
+    coordinate on edge of the board.
+
+    Pre : grid is of class Grid, coord and dir are integers
+          dir = 0 means movement in x direction, dir = 1 means y direction
+    Post: returns resulting coordinate as integer
+    """
+
+    # change coord if it is currently outside of grid size
     if coord < 0:
         coord = 0
     elif coord > grid.size_grid()[dir]:
--- docs/Individueel onderdeel Miguel.md
+++ docs/Individueel onderdeel Miguel.md
@@ -15,3 +15,60 @@ Ik kan vervolgens ook testen of het algoritme een beter antwoord krijg als ik he
 ## Aansluiting
 
 Het programma sluit aan op het werk van de rest van het groep, doordat het één van de algoritmen is die een zo goed mogelijk antwoord probeert te zoeken voor onze casus. Het maakt natuurlijk gebruik van de reeds gecreëerde classes en de visualisatie, opdat je van het resultaat kan zien hoe goed de oplossing er intuitief uitziet. Natuurlijk kunnen de oplossingen van dit algoritme ook gebruikt worden om onze andere algoritmen mee te vergelijken, om te kijken hoe effectief ze onderling zijn
+
+## Gedeeltes anders dan verwacht
+
+Voor de temperatuur functie had ik origineel bedacht dat de temperatuur met een factor tot de macht van de iteratie zou verlagen. Bijvoorbeeld T = 500 * (0.997 ^ iteratie). Echter na de oefenpresentatie werd ons aangeraden om voor de temperatuur functie te zorgen dat de nieuwe oplossing nooit 10% slechter kan zijn dan de oplossing waar we mee begonnen. Ook werd opgemerkt dat met deze temperatuur functie, je na ongeveer 1800 iteraties gewoon een hill climber over hield. Om deze reden heb ik ervoor gezorgd dat bij iedere duizendste iteratie, de temperatuur wat omhoog wordt gebumpt, in de hoop dat de oplossing in een lager locaal minimum terecht komt.
+
+## Links naar commits
+
+https://github.com/sbritton1/We-are-trying/commit/a1b96d0e247985c5470a3f1e1973179a5170f33c:
+Algoritme voor het grootste gedeelte werkend gemaakt
+
+https://github.com/sbritton1/We-are-trying/commit/ab6b80b0edd19a0409b5522f557a6f2aef306c09:
+Algoritme paar keer gerunt
+
+https://github.com/sbritton1/We-are-trying/commit/d2d09914c55e3b72716ce3679069f15c4af1daa0:
+Plot toegevoegd van cost over tijd
+
+https://github.com/sbritton1/We-are-trying/commit/5836e67ddcc065f376d46fa4450e475fdbfa1b3f:
+Plot verbeterd met titels etc
+
+https://github.com/sbritton1/We-are-trying/commit/c97054257135b2b257e3736da773e1c089717d12:
+Multiprocessing toegevoegd, om runtime veel te verlagen
+
+https://github.com/sbritton1/We-are-trying/commit/1949effc774a0a76a7f4531091f5ac8c0be222c9:
+Testen met temperatuurfunctie
+
+https://github.com/sbritton1/We-are-trying/commit/ec824c08c726f0818d67d5cece31ed1eeec35413:
+Aparte helperfuncties gemaakt van sommige functionaliteiten, die in meer algoritmes gebruikt worden
+
+https://github.com/sbritton1/We-are-trying/commit/a190baec72cdac6a33b94cb3b77f22a6c1c78d00:
+Vaker algoritme gerunt en temperatuursfunctie terug aangepast
+
+https://github.com/sbritton1/We-are-trying/commit/3ccc9e6bdaab32483f0ecb2c7538bb4d54e32e62:
+Comments en docstrings toegevoegd
+
+https://github.com/sbritton1/We-are-trying/commit/9a92f954d90883ae6cb794ccd963529368be797a:
+Comments en docstrings verbeterd
+
+https://github.com/sbritton1/We-are-trying/commit/319bb890f56a30e5fb08db6d2719a433a7451c25: 
+Meer helperfuncties gemaakt, zodat de .py bestanden van de algoritmes kleiner zijn en herhaling wordt voorkomen
+
+https://github.com/sbritton1/We-are-trying/commit/4a24147a46c6765cdcd6aff1cf0a396fd20feed3: 
+Met behulp van flake8 de stijl verbeterd
+
+https://github.com/sbritton1/We-are-trying/commit/7ecce77922d533cde0ebc99d629926d8441431fc: 
+Grote test gerund, dus aangepast hoevaak het algoritme een grid probeert te verbeteren
+
+https://github.com/sbritton1/We-are-trying/commit/b7a6067d8c333d6b005064f63862cba8cfcbe852: 
+Simulated annealing uitgetest met greedy oplossing als beginpunt in plaats van random beginpunt
+
+https://github.com/sbritton1/We-are-trying/commit/0b4dada880b9235bf139c7f12fa448182b04d93a:
+Betere temperatuursfunctie uitproberen
+
+https://github.com/sbritton1/We-are-trying/commit/bad0ed0b36afeab5e57cc5ed69eafcedc7b5cce9:
+Nieuwe temperatuursfunctie gebruikt die goed lijkt te werken, ook snelheid verbeterd door het algoritme minder onnodige dingen te laten doen
+
+https://github.com/sbritton1/We-are-trying/commit/560871b42ea7422eaeefdf4f6306f926271d3502:
+Grote test met random starting grid
